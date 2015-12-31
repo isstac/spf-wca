f40650f586304357abeedf21f60d42cba92c052f
Kasper Luckow <ksluckow@gmail.com>
2015-12-31 17:55:10 +0100

    add interfaces

---
+++ src/main/wcanalysis/heuristic/CSVable.java
@@ -0,0 +1,10 @@
+package wcanalysis.heuristic;
+
+/**
+ * @author Kasper Luckow
+ * TODO: consider refactoring this
+ */
+public interface CSVable {
+  public String getCSVHeader();
+  public String getCSV();
+}
---
+++ src/main/wcanalysis/heuristic/Policy.java
@@ -0,0 +1,12 @@
+package wcanalysis.heuristic;
+
+import java.io.Serializable;
+
+/**
+ * @author Kasper Luckow
+ *
+ */
+public abstract class Policy implements Serializable {
+  private static final long serialVersionUID = -2247935610676857237L;
+  
+}
