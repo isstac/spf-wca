5aa17038e16a633a10822b1598ca22f206a8d98b
Kasper Luckow <ksluckow@gmail.com>
2017-09-15 11:26:37 -0700

    Transition to JfreeChart instead of Xchart

--- src/main/wcanalysis/fitting/FunctionFitter.java
+++ src/main/wcanalysis/fitting/FunctionFitter.java
@@ -24,24 +24,17 @@
 
 package wcanalysis.fitting;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import org.apache.commons.math3.exception.MathIllegalArgumentException;
+
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
-import org.apache.commons.math3.exception.MathIllegalArgumentException;
-
 import gov.nasa.jpf.util.JPFLogger;
-import wcanalysis.charting.DataCollection;
 import wcanalysis.charting.DataSeries;
 
 /**
@@ -52,15 +45,8 @@ public class FunctionFitter {
   
   public static final Logger logger = JPFLogger.getLogger(FunctionFitter.class.getName());
   
-  public static Collection<DataSeries> computeSeries(DataCollection rawData, int predictionModelSize) {
-    Collection<DataSeries> series = new HashSet<>();
-
-
-    DataSeries rawSeries = new DataSeries("Raw");
-    double[] rxs = rawData.getX();
-    double[] rys = rawData.getY();
-    for(int i = 0; i < rawData.size(); i++) //ugly conversion and ugly non-iterable
-      rawSeries.add(rxs[i], rys[i]);
+  public static Collection<DataSeries> computePredictionSeries(double[] xs, double[] ys,
+                                                     int predictionModelSize) {
 
     DecimalFormat df = new DecimalFormat("#.00000");
     List<TrendModelData> trendLines = new ArrayList<>();
@@ -78,11 +64,13 @@ public class FunctionFitter {
     HashMap<TrendModelData, DataSeries> trend2series = new HashMap<>();
     
     Iterator<TrendModelData> tmIter = trendLines.iterator();
+
+    // Create data series
     while(tmIter.hasNext()) {
       TrendModelData trendData = tmIter.next();
       try {
-        trendData.trendLine.setValues(rawData.getY(), rawData.getX());
-        DataSeries s = new DataSeries(trendData.desc);
+        trendData.trendLine.setValues(ys, xs);
+        DataSeries s = new DataSeries(trendData.desc, predictionModelSize);
         s.setFunction(trendData.trendLine.getFunction());
         s.setR2(df.format(trendData.trendLine.getRSquared()));
 
@@ -94,7 +82,6 @@ public class FunctionFitter {
     }
 
     double[] xPredict = new double[predictionModelSize];
-    double[] xs = rawData.getX();
     System.arraycopy(xs, 0, xPredict, 0, xs.length);
     for(int i = xs.length; i < predictionModelSize; i++)
       xPredict[i] = xPredict[i-1] + 1.0;
@@ -110,25 +97,27 @@ public class FunctionFitter {
       }
     }
 
-    series.add(rawSeries);
-    for(DataSeries s : trend2series.values()) {
-      series.add(s);
-    }
-    return series;
-  }
-  
-  private static final Object[] FILE_HEADER = {"x","y"};
+    return trend2series.values();
 
-  public static void seriesToCSV(DataSeries series, Writer w) throws IOException {
-    
-    CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator("\n");
-    try(CSVPrinter csvFilePrinter = new CSVPrinter(w, csvFileFormat)) {
-      csvFilePrinter.printRecord(FILE_HEADER);
-      double[] x = series.getX();
-      double[] y = series.getY();
-      for(int i = 0; i < x.length; i++) {
-        csvFilePrinter.printRecord(x[i], y[i]);
-      }
-    }
+//    Collection<XYSeries> predictionSeries = new HashSet<>();
+//    // Add raw data
+//    XYSeries rawSeries = new XYSeries("raw");
+//    for(int i = 0; i < xs.length; i++) {
+//      rawSeries.add(xs[i], ys[i]);
+//    }
+//    predictionSeries.add(rawSeries);
+//
+//    // Add all the other series
+//    for(DataSeries s : trend2series.values()) {
+//      XYSeries jfreeSeries = new XYSeries(s.getSeriesName() + ": " +
+//          s.getFunction() + "(r^2=" + s.getR2() + ")");
+//      double xss[] = s.getX();
+//      double yss[] = s.getY();
+//      for(int i = 0; i < xss.length; i++) {
+//        jfreeSeries.add(xss[i], yss[i]);
+//      }
+//      predictionSeries.add(jfreeSeries);
+//    }
+//    return predictionSeries;
   }
 }
